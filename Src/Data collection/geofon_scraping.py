eb04d357ba5e80d512bf59c53cca97b966d4f99f
Arash <arash.alikhani.dev@gmail.com>
2026-07-27 22:40:15 +0330

    first geofon scraping

--- Src/Data collection/geofon_scraping.py
+++ Src/Data collection/geofon_scraping.py
@@ -1,1 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
 
+import pandas as pd
+
+
+# import numpy as np
+#-------------------------//-------------------------------
+
+# page url for earthquake data
+url = "https://geofon.gfz.de/eqinfo/form.php"
+
+page = requests.get(url)
+
+
+#  html parser
+soup = BeautifulSoup(page.text, "html.parser")
+
+tables = soup.find_all("table")
+
+print(tables)
+
+# save as csv file
+df = pd.read_html(str(tables))[0]
+df.to_csv("Data/CSV/JAPAN_GEOFON.csv", index=False)
