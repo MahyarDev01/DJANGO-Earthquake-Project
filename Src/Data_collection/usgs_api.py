b7d06145355844b165ba350ef578d97ea9215671
faeze <faezehr.95@gmail.com>
2026-07-31 17:59:18 +0330

    modified

--- Src/Data_collection/usgs_api.py
+++ Src/Data_collection/usgs_api.py
@@ -1,28 +1,44 @@
 import requests
 from datetime import datetime, timedelta
+from configs import JAPAN_USGS_CSV
 
-end_date = datetime.today().date()
-start_date = end_date - timedelta(days=30)
 
-url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+    
+def build_params():
 
-params = {
-    "format": "csv",
-    "starttime": str(start_date),
-    "endtime": str(end_date),
-    "minlatitude": 24,
-    "maxlatitude": 46,
-    "minlongitude": 123,
-    "maxlongitude": 146,
-    "minmagnitude": 1
-}
+    end_date = datetime.today().date()
+    start_date = end_date - timedelta(days=30)
 
-response = requests.get(url, params=params)
-response.raise_for_status()
+    params = {
+        "format": "csv",
+        "starttime": str(start_date),
+        "endtime": str(end_date),
+        "minlatitude": 24,
+        "maxlatitude": 46,
+        "minlongitude": 123,
+        "maxlongitude": 146,
+        "minmagnitude": 1
+    }
+    return params
 
-file_path = './Data/CSV/JAPAN_USGS.csv'
+def download_usgs_data(params):
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
-with open(file_path, "w", encoding="utf-8") as f:
-    f.write(response.text)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response 
 
-print("USGS Done !!")
+def save_csv(response, file_path):
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(response.text)
+
+
+def collect_usgs_data():
+        params = build_params()
+        response = download_usgs_data(params)
+        save_csv(response, JAPAN_USGS_CSV)
+        print("USGS Done !!")
+
+if __name__ == '__main__':
+     collect_usgs_data()
